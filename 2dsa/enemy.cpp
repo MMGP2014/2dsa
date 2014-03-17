f6eb0f2b10565c6dcef05a7336ce26caecb00fe2
louie <r96wii@gmail.com>
2014-03-17 16:55:10 +0900

    あ

--- 2dsa/enemy.cpp
+++ 2dsa/enemy.cpp
@@ -15,7 +15,10 @@ void input_floor_enemy(FLOOR *flr,char *fname,ENEMY *ene){
 		sscanf_s(buf,"%d,%d,%d",&flr->enemy[cnt].type,&flr->enemy[cnt].crd.x,&flr->enemy[cnt].crd.y);
 		flr->enemy[cnt].HP=(ene+flr->enemy[cnt].type)->HP;
 		flr->enemy[cnt].size=(ene+flr->enemy[cnt].type)->size;
+		flr->enemy[cnt].handle=(ene+flr->enemy[cnt].type)->handle;
 	}
+	cnt++;
+	flr->enemy_num=cnt;
 	fclose(fp);
 }
 
@@ -27,7 +30,7 @@ void file_in(ENEMY *ene,char *file){
 	char s[256];
 	int i;
 	i = 0;
-
+	char file_name[1000];
 	if((fopen_s(&fp,file,"r"))!=0){
 		printf("ERORRRRRRR!");
 		exit(EXIT_FAILURE);
@@ -43,7 +46,9 @@ void file_in(ENEMY *ene,char *file){
 		
 	}
 	sscanf_s(s,"%d,%d,%d,&d",&(ene+i-2)->type,&(ene+i-2)->size.x,&(ene+i-2)->size.y,&(ene+i-2)->HP);	
-
+	sprintf_s(file_name,"%senemy_type%d.png",PIC,(ene+i-2)->type);
+	(ene+i-2)->handle = LoadGraph(file_name);
+	
 	/*ClearDrawScreen();
 		print_char(s);
 		print_int(i);
